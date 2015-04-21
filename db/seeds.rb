1555eac25f2e34b7ee049c1cb7fbf321e9d9482d
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-04-20 22:17:16 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+cities = [
+  "Austin",
+  "Columbus",
+  "London",
+  "New York City",
+  "Paris",
+  "Philadelphia",
+  "Portland",
+  "San Francisco"
+]
+
+cities_length = cities.length
+
+(0...1000).each do
+  city_index = (rand * cities_length).floor
+  User.create({
+    username: Faker::Internet.user_name,
+    password: Faker::Internet.password,
+    city: cities[city_index],
+    profession: Faker::Name.title
+  })
+end
+
+User.create(
+  username: "user",
+  password: "pass",
+  city: "San Francisco",
+  profession: "Mail delivery agent"
+)
